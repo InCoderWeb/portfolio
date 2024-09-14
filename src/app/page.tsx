dd43a91f1ee0683a8b65ff62523a60d4c45c92d5
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2024-09-14 22:16:29 +0530

    created home section of portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-// import { FlipWords } from "@/components/ui/flip-words";
-// import { heroFlipTexts } from "@/helpers/constants";
+import { FlipWords } from "@/components/ui/flip-words";
+import { heroFlipTexts } from "@/helpers/constants";
+
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 export default function Home() {
 	return (
@@ -7,9 +9,16 @@ export default function Home() {
 			<section className="hero">
 				<div className="container">
 					<div className="left">
-						{/* <FlipWords words={heroFlipTexts} duration={2000} className="text-white" /> */}
+						<p className="text-lg text-white/90">Hi 👋, my name is</p>
+						<h1 className="text-[3rem] md:text-[3.5rem] playpen-sans-regular">Ashutosh Kumar</h1>
+						<h1 className="text-[1.5rem] text-white/90 tracking-wider">I'm a<FlipWords words={heroFlipTexts} duration={2000} className="text-white" /></h1>
+						<p className="mt-1 w-[25rem] text-white/50 tracking-wide">An enthusiastic programmer, tech enthusiast, and creative problem solver passionate about building innovative solutions and mastering the latest technologies.</p>
 					</div>
-					<div className="right"></div>
+					<div className="right">
+						<BackgroundLines>
+							<img src={"/assets/images/hero.png"} />
+						</BackgroundLines>
+					</div>
 				</div>
 			</section>
 		</>
