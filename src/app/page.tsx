31bea5766e851861d8b12519660006c79943e5f5
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2024-09-14 22:18:50 +0530

    resolved deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 					<div className="left">
 						<p className="text-lg text-white/90">Hi 👋, my name is</p>
 						<h1 className="text-[3rem] md:text-[3.5rem] playpen-sans-regular">Ashutosh Kumar</h1>
-						<h1 className="text-[1.5rem] text-white/90 tracking-wider">I'm a<FlipWords words={heroFlipTexts} duration={2000} className="text-white" /></h1>
+						<h1 className="text-[1.5rem] text-white/90 tracking-wider">I&apos;m a<FlipWords words={heroFlipTexts} duration={2000} className="text-white" /></h1>
 						<p className="mt-1 w-[25rem] text-white/50 tracking-wide">An enthusiastic programmer, tech enthusiast, and creative problem solver passionate about building innovative solutions and mastering the latest technologies.</p>
 					</div>
 					<div className="right">
