aeff84d772b60b26ebaaa74cbef0f82e20c82c55
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2024-09-15 20:28:22 +0530

    completed about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,73 @@
-import { FlipWords } from "@/components/ui/flip-words";
-import { heroFlipTexts } from "@/helpers/constants";
+// import { FlipWords } from "@/components/ui/flip-words";
+// import { heroFlipTexts } from "@/helpers/constants";
+"use client";
 
-export default function Home() {
+import { Variants, motion } from "framer-motion";
+
+const fadeInUpAnimation: Variants = {
+	hidden: {
+		opacity: 0,
+		y: 100,
+	},
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			staggerChildren: 0.3,
+			duration: 1.5,
+		},
+	},
+};
+
+const zoomUpAnimation: Variants = {
+	hidden: {
+		scale: 0.8,
+	},
+	show: {
+		scale: 1,
+		transition: {
+			staggerChildren: 0.3,
+			duration: 1.5,
+		},
+	},
+};
+
+export default function About() {
 	return (
 		<>
-			<section className="hero">
+			<section className="about pb-[2rem] md:pb-0 mx-[2rem] md:mx-0">
 				<div className="container">
-					<div className="left">
-						<FlipWords words={heroFlipTexts} duration={2000} className="text-white" />
-					</div>
-					<div className="right"></div>
+					<motion.div
+						className="left mt-[4rem] md:mt-0"
+						initial="hidden"
+						animate="show"
+						variants={fadeInUpAnimation}
+					>
+						{/* <FlipWords
+							words={heroFlipTexts}
+							duration={2000}
+							className="text-white"
+						/> */}
+						<motion.h1
+							className="text-[3rem] font-semibold tracking-wider playpen-sans-regular"
+							variants={fadeInUpAnimation}
+						>
+							About Me
+						</motion.h1>
+						<motion.p variants={fadeInUpAnimation} className="mt-2 mx-[2rem] md:mx-0 tracking-widest text-white/60 max-w-[40rem] text-[0.9rem]">
+							Hello, I'm Ashutosh Kumar, currently pursuing an Integrated Master of Computer Applications from Acropolis Institute of Technology and Research, Indore, India. With a strong foundation in				problem-solving and a collaborative mindset, I am passionate about developing cutting-edge solutions 		that blend innovation with practicality. I enjoy working in team environments, where I can leverage
+							my communication skills and technical expertise to contribute meaningfully to projects. Whether it's
+							coding, tackling new challenges, or learning the latest technologies, I&apos;m always eager to grow and
+							push boundaries. Let&apos;s connect and explore possibilities!
+						</motion.p>
+					</motion.div>
+					<motion.div className="right" initial="hidden"
+						animate="show"
+						variants={zoomUpAnimation}>
+						<div className="imageContainer w-fit max-h-fit rounded-full overflow-hidden z-[9999]">
+							<motion.img variants={zoomUpAnimation} src="/assets/images/me.jpeg" />
+						</div>
+					</motion.div>
 				</div>
 			</section>
 		</>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,111 @@
+"use client";
 import { FlipWords } from "@/components/ui/flip-words";
 import { heroFlipTexts } from "@/helpers/constants";
 
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { Button } from "@/components/ui/button";
 import { CircleDot, CopyCheckIcon } from "lucide-react";
+import { motion, Variants } from "framer-motion";
+
+const fadeInUpAnimation: Variants = {
+	hidden: {
+		opacity: 0,
+		y: 100,
+	},
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			staggerChildren: 0.3,
+			duration: 1.5,
+		},
+	},
+};
+
+const zoomUpAnimation: Variants = {
+	hidden: {
+		scale: 0.8,
+	},
+	show: {
+		scale: 1,
+		transition: {
+			staggerChildren: 0.3,
+			duration: 1.5,
+		},
+	},
+};
 
 export default function Home() {
 	return (
 		<>
 			<section className="hero">
 				<div className="container mx-10">
-					<div className="left z-[9]">
-						<p className="text-lg text-white/90">Hi 👋, my name is</p>
-						<h1 className="text-[3rem] md:text-[3.4rem] playpen-sans-regular">Ashutosh Kumar</h1>
-						<h1 className="text-[1.5rem] text-white/90 tracking-wider">I&apos;m a<FlipWords words={heroFlipTexts} duration={2000} className="text-white" /></h1>
-						<p className="mt-1 w-[25rem] text-white/50 tracking-wide">An enthusiastic programmer, tech enthusiast, and creative problem solver passionate about building innovative solutions and mastering the latest technologies.</p>
+					<motion.div
+						initial="hidden"
+						animate="show"
+						variants={fadeInUpAnimation}
+						className="left z-[9]"
+					>
+						<motion.p
+							variants={fadeInUpAnimation}
+							className="text-lg text-white/90"
+						>
+							Hi 👋, my name is
+						</motion.p>
+						<motion.h1
+							variants={fadeInUpAnimation}
+							className="text-[3rem] md:text-[3.4rem] playpen-sans-regular"
+						>
+							Ashutosh Kumar
+						</motion.h1>
+						<motion.h1
+							variants={fadeInUpAnimation}
+							className="text-[1.5rem] text-white/90 tracking-wider"
+						>
+							I&apos;m a
+							<FlipWords
+								words={heroFlipTexts}
+								duration={2000}
+								className="text-white"
+							/>
+						</motion.h1>
+						<motion.p
+							variants={fadeInUpAnimation}
+							className="mt-1 w-[25rem] text-white/50 tracking-wide"
+						>
+							An enthusiastic programmer, tech enthusiast, and
+							creative problem solver passionate about building
+							innovative solutions and mastering the latest
+							technologies.
+						</motion.p>
 
-						<div className="mt-4 flex justify-start items-center">
-							<Button className="heroBtn"><CircleDot className="size-4 mr-2" /> Hire Me</Button>
-							<Button className="heroBtn"><CopyCheckIcon className="size-4 mr-2" /> Copy Email</Button>
-						</div>
-					</div>
-					<div className="right">
+						<motion.div
+							variants={fadeInUpAnimation}
+							className="mt-4 flex justify-start items-center"
+						>
+							<Button className="heroBtn">
+								<CircleDot className="size-4 mr-2" /> Hire Me
+							</Button>
+							<Button className="heroBtn">
+								<CopyCheckIcon className="size-4 mr-2" /> Copy
+								Email
+							</Button>
+						</motion.div>
+					</motion.div>
+					<motion.div
+						initial="hidden"
+						animate="show"
+						variants={zoomUpAnimation}
+						className="right"
+					>
 						<BackgroundLines>
-							<img src={"/assets/images/hero.png"} alt="My Bitmoji" />
+							<motion.img
+								variants={zoomUpAnimation}
+								src={"/assets/images/hero.png"}
+								alt="My Bitmoji"
+							/>
 						</BackgroundLines>
-					</div>
+					</motion.div>
 				</div>
 			</section>
 		</>
